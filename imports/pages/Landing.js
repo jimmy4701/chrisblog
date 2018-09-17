d6a05a8a049797be695d1df3030316a102412848
Jimmy <jeremy.simonklein@gmail.com>
2018-09-17 18:56:50 +0200

    Added inputs on landing

--- imports/pages/Landing.js
+++ imports/pages/Landing.js
@@ -10,7 +10,9 @@ export default class Landing extends Component {
 
     birthday = () => this.setState({age: this.state.age + 1})
 
-    changeTitle = (e) => this.setState({title: e.target.value})
+    handleChange = (e) => this.setState({
+        [e.target.name]: e.target.type == "number" ? parseFloat(e.target.value) : e.target.value
+    })
     
     render(){
         const { title, description, age } = this.state
@@ -23,7 +25,9 @@ export default class Landing extends Component {
                 <p>Chris est un jeune développeur de {age} ans</p>
                 <button onClick={this.birthday}>Joyeux anniversaire !</button>
                 <form>
-                    <input type="text" value={title} onChange={this.changeTitle} />
+                    <input type="text" name="title" value={title} onChange={this.handleChange} />
+                    <input type="text" name="description" value={description} onChange={this.handleChange} />
+                    <input type="number" name="age" value={age} onChange={this.handleChange} />
                 </form>
             </div>
         )
