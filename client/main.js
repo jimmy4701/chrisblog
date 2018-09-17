bfda2ebe53f1d1e3c046c7d776759bc60c3bfd35
Jimmy <jeremy.simonklein@gmail.com>
2018-09-17 17:55:47 +0200

    Installed react and react-dom Created Landing component

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 Meteor.startup(() => {
     console.log('CLIENT: LAUNCH')
+    ReactDOM.render(
+        <h1>EOZIFO ZE</h1>,
+        document.getElementById('root')
+    )
 })
